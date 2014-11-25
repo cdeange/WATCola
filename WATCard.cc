bececa1e8daaa36a3d78f089b34eab2bee9d3b55
Dian Xiang <dian.xiang1@gmail.com>
2014-11-25 03:14:11 -0500

    watcard cpp remove

--- WATCard.cc
+++ WATCard.cc
@@ -1,3 +1,25 @@
 #include "WATCard.h"
 
-using namespace std;
+WATCard::WATCard( const WATCard & ) {}
+
+WATCard& WATCard::operator=( const WATCard & watcard) {
+  throw 1; 
+}
+
+WATCard::WATCard() {
+  mBalance = 0;
+}
+
+void WATCard::deposit( unsigned int amount ) {
+  mBalance += amount;
+}
+
+// TODO do we need to check things here? 
+// Assumes whatever calls it determines whether there is enough
+void WATCard::withdraw( unsigned int amount ) {
+  mBalance -= amount;
+}
+
+unsigned int WATCard::getBalance() {
+  return mBalance;
+}
