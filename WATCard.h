f7c81f150ac73d6a2734d6e88540369dbad9ad9c
Dian Xiang <dian.xiang1@gmail.com>
2014-11-25 00:36:01 -0500

    adding watcard shits

--- WATCard.h
+++ WATCard.h
@@ -1,9 +1,13 @@
 #ifndef WATCARD_H_
 #define WATCARD_H_
 
+#include "uFuture.h"
+
 class WATCard {
     WATCard( const WATCard & );            // prevent copying
     WATCard &operator=( const WATCard & );
+
+    unsigned int mBalance;
   public:
     WATCard();
     typedef Future_ISM<WATCard *> FWATCard; // future watcard pointer
