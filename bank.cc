8fc92286572c510cc778e91935e1fdff7ce7e615
Dian Xiang <dian.xiang1@gmail.com>
2014-11-24 23:35:01 -0500

    initial bank files

---
+++ bank.cc
@@ -0,0 +1,19 @@
+#include "bank.h"
+
+Bank::Bank( unsigned int numStudents ) {
+  for( unsigned int i = 0; i < numStudents; i++ ) {
+    mBalances.push_back(0);
+  }
+}
+
+void Bank::deposit( unsigned int id, unsigned int amount ) {
+  mBalances[id] += amount;
+}
+
+void Bank::withdraw( unsigned int id, unsigned int amount ) {
+  while( mBalance[id] < amount ) {
+    _Accept(deposit);
+  }
+  
+  mBalances[id] -= amount;
+}
